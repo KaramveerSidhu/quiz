e38a29ea216fe17a262801bb302ccfc10c668b12
KaramveerSidhu <karamveersidhu14@gmail.com>
2020-12-30 11:51:37 +0530

    Uploading files

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <b-nav tabs>
+      <b-nav-item disabled>Sports Quiz</b-nav-item>
+      <b-nav-item disabled>Counter {{numCorrect}}/{{numTotal}}</b-nav-item>
+    </b-nav>
+  </div>
+</template>
+
+<script type="text/javascript">
+  
+  export default {
+    props: [
+      'numCorrect',
+      'numTotal'
+    ]
+  }
+
+</script>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,112 @@
+<template>
+    <div>
+      <b-jumbotron>
+        <template #lead>
+            {{ currentQuestion.question }}
+        </template>
+
+        <hr class="my-4">
+
+        <b-list-group>
+          <b-list-group-item v-for="(answer, index) in answers" :key="index" @click="selectAnswer(index)" 
+            :class="[selectedIndex === index? 'selected' : '']"
+          >
+            {{answer}} 
+          </b-list-group-item>
+        </b-list-group>
+
+        <b-button variant="primary" @click="submitAnswer" :disabled="selectedIndex === null">Submit </b-button>
+        <b-button @click="next" variant="success" href="#">Navigate</b-button>
+      </b-jumbotron>
+    </div>
+</template>
+
+<style scoped>
+    .list-group {
+        margin-bottom: 15px;
+    }
+
+    .list-group-item:hover {
+        background: #EEE;
+        cursor: pointer;
+    }
+
+    .btn {
+        margin: 0 5px;
+    }
+
+    .selected {
+        background-color: lightblue;
+    }
+
+    .correct {
+        background-color: lightgreen;
+    }
+
+    .incorrect {
+        background-color: red;
+    }
+
+</style>
+
+<script type="text/javascript">
+
+import _ from 'lodash'
+    
+    export default {
+        props: {
+            currentQuestion: Object,
+            next: Function,
+            increment: Function
+        },
+
+        data() {
+            return {
+                selectedIndex: null,
+                shuffledAnswers: []
+            }
+        },
+
+        computed: {
+            answers() {
+                let answers = [...this.currentQuestion.incorrect_answers]
+                answers.push(this.currentQuestion.correct_answer)
+                return answers
+            }
+        },
+
+        watch: {
+            currentQuestion: {
+                immediate: true,
+                handler() {
+                    this.selectedIndex = null
+                    this.shuffleAnswers()
+                }
+            }
+        },
+
+        methods: {
+            selectAnswer(index) {
+                this.selectedIndex = index
+            },
+
+            submitAnswer() {
+                let isCorrect =  false
+
+                if (this.selectedIndex === this.correctIndex) {
+                    isCorrect = true
+                }
+
+                this.increment(isCorrect)
+            },
+
+            shuffleAnswers() {
+              let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]  
+              this.shuffledAnswers = _.shuffle(answers)
+            },
+        },
+
+    }
+
+</script>
+
